4d9a3d39e6f9d134f642435ac16de8a377090f2e
AverageSecurityGuy <stephen@averagesecurityguy.info>
2016-02-26 09:45:06 -0500

    Encoding not needed. Modify settings.

--- generate_ec2_conf.py
+++ generate_ec2_conf.py
@@ -8,21 +8,23 @@ git pull of the masscan github repository to get the latest exclude.conf file.
 """
 import requests
 
-RATE = 5000.00
-OUTFILE = 'ec2.list'
-PORT = '6379'
+RATE = 150000.00
+FORMAT = 'list'
+PORT = '445'
+OUTFILE = 'ec2_{0}.list'.format(PORT)
+BANNER = 'false'
 
 resp = requests.get('https://ip-ranges.amazonaws.com/ip-ranges.json')
 ranges = resp.json()
-
 ec2 = [r['ip_prefix'] for r in ranges['prefixes'] if r['service'] == 'EC2']
 
-with open('ec2.conf', 'wb') as f:
-    f.write('# EC2 Masscan Configuration.\n'.encode('utf-8'))
-    f.write('rate = {0}\n'.format(RATE).encode('utf-8'))
-    f.write('output-format = list\n'.encode('utf-8'))
-    f.write('output-filename = {0}\n'.format(OUTFILE).encode('utf-8'))
-    f.write('port = {0}\n'.format(PORT).encode('utf-8'))
-    f.write('exclude-file = exclude.conf\n'.encode('utf-8'))
+with open('ec2.conf', 'w') as f:
+    f.write('# EC2 Masscan Configuration.\n')
+    f.write('rate = {0}\n'.format(RATE))
+    f.write('output-format = {0}\n'.format(FORMAT))
+    f.write('output-filename = {0}\n'.format(OUTFILE))
+    f.write('port = {0}\n'.format(PORT))
+    f.write('exclude-file = exclude.conf\n')
+    f.write('banners = {0}\n'.format(BANNER))
     for r in ec2:
-        f.write('range = {0}\n'.format(r).encode('utf-8'))
+        f.write('range = {0}\n'.format(r))
